962e0d03f600d6d500d58f1a8af03be4a5e23bf7
dondadharmik <142986999+dondadharmik@users.noreply.github.com>
2024-01-08 14:44:11 +0530

    Update TodoMain.js

--- src/component/TodoMain.js
+++ src/component/TodoMain.js
@@ -164,7 +164,7 @@ const TodoMain = () => {
           })}
          
         </div>          
-        <div className={classes.footer}> <p>&copy; 2023 TODO PILOT</p></div>
+        <div className={classes.footer}> <p>&copy; 2024 TODO PILOT</p></div>
 
       </div>
       <BackToTopButton/>
